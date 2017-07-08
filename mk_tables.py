83b5a6e344a0fa4d563c5f552d950c3224b85528
ml9951 <ml9951@rit.edu>
2017-07-08 13:08:27 -0600

    Adding more slides

--- mk_tables.py
+++ mk_tables.py
@@ -13,31 +13,14 @@ def renameCol(col):
     col = col.split('/')[-1]
     return re.sub(' +', '_', col.lower())
 
-def mk_case_data_table():
-    id = '988058bd3248104e3114ecac4b3fc674'
-    df = downloadFile(id, 'case_data', 'atlas-api-demo')
-    df.to_sql('case_data', engine, if_exists='replace', index=False, schema='tula')
-
 if __name__ == '__main__':
-
-    for file in glob.glob('data/*.xlsx'):
-        df = pandas.read_excel(file)
+    # Get all .xlsx files
+    for file in glob.glob('data/*.xlsx'): 
+        # Import the file into a Dataframe
+        df = pandas.read_excel(file) 
+        #Create PostgreSQL compliant names
         df.columns = map(renameCol, df.columns)
         table_name = renameCol(os.path.splitext(os.path.basename(file))[0])
         print('Creating %s' % table_name)
+        #Write the dataframe to PostgreSQL
         df.to_sql(table_name, engine, if_exists='replace', index=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
