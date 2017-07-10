3b33508efe02bbecd683efaa8bf8b0b89a9dd1fe
ml9951 <ml9951@rit.edu>
2017-07-10 17:21:45 -0600

    Updating demo to update database

--- commcare.py
+++ commcare.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-import requests
-import pdb
-import json
+import requests, pdb, json, re
+from time import sleep
 
 BASE = 'https://www.commcarehq.org'
 
@@ -13,11 +12,22 @@ def encodeParams(filter):
         params = map(lambda p: '%s=%s' % (p, filter[p]), filter.keys())
         return '?%s' % '&'.join(params)
 
+# Rename columns to something that is PostgreSQL compliant
+def renameCol(col):
+    col = col.split('/')[-1]
+    return re.sub(' +', '_', col.lower())
+
 class Commcare:
     def __init__(self, username, password, project):
         self.project = project
         self.session = requests.Session()
         self.session.auth = (username, password)
+
+    def mkDateRange(self, filter_name, begin, end):
+        return {
+            '%s-start' % filter_name : str(begin).split(' ')[0],
+            '%s-end' % filter_name : str(end).split(' ')[0]
+        }
 
     def listForms(self):
         req = self.session.get('%s/a/%s/api/v0.5/simplereportconfiguration/?format=json' % (BASE, self.project))
@@ -35,6 +45,9 @@ class Commcare:
         while url != '':
             req = self.session.get('%s/%s' % (BASE, url))
             if req.status_code != 200:
+                if req.status_code == 429: #Throttling
+                    sleep(1)
+                    continue
                 pdb.set_trace()
                 print(req.text)
                 raise Exception(req.text)
@@ -45,6 +58,13 @@ class Commcare:
 
             total += len(body['data'])
 
-            yield body['data'], body['columns']
+            data = body['data']
+            cols = body['columns']
+
+            colMap = {}
+            for rec in cols:
+                colMap[rec['slug']] = renameCol(rec['header'])
+
+            yield [{colMap[k] : None if r[k] == '' else r[k] for k in r.keys()} for r in data]
             url = body['next_page']
 
--- demo.py
+++ demo.py
@@ -2,20 +2,55 @@
 
 from commcare import Commcare
 from getpass import getpass
-import json
-import argparse
+import psycopg2, pdb, json, argparse
+from datetime import datetime
+from psycopg2.extensions import AsIs
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--reports', action='store_true', help='List all reports in a JSON file')
+parser.add_argument('--update', action='store_true', help='Update the database')
 args = parser.parse_args()
+
+conn = psycopg2.connect(dbname='commcare-demo')
+cursor = conn.cursor()
 
 # Get the user's password and create the Commcare client.
 pw = getpass('Enter password: ')
 client = Commcare('matthew.le@mssm.edu', pw, 'atlas-api-demo')
 
+def updateTable():
+	# Fetch the neweset dates
+	cursor.execute('SELECT MAX(modified_on) FROM gps_case_data')
+	start_date, = cursor.fetchone()
+	date_filter = None
+	if start_date is not None:
+		date_filter = client.mkDateRange(start_date, datetime.now(), 'modified_on_6457b79c_2')
+
+	for rows in client.getReport('988058bd3248104e3114ecac4b3fc674', filter=date_filter):
+		cursor.executemany("""
+			INSERT INTO gps_case_data(
+				name, 
+				gps,
+				latitude, 
+				longitude, 
+				altitude, 
+				precision, 
+				modified_on
+			)
+			VALUES (
+				%(name)s,
+				%(gps)s,
+				%(latitude)s,
+				%(longitude)s,
+				%(altitude)s,
+				%(precision)s,
+				%(modified_on)s
+			)
+		""", rows)
+	conn.commit()
+
 if args.reports:
     report = client.listForms()
     json.dump(report, open('reports.json', 'w'), indent=2)
-
-
-
+elif args.update:
+	updateTable()
